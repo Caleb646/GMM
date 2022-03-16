a0cada9330bba12d26c3dc9efd7d8027ab8c8aad
Caleb <calebthomas646@yahoo.com>
2022-03-16 11:12:26 -0500

    before deploy

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#python manage.py makemigrations rfis
+python manage.py makemigrations rfis
 python manage.py makemigrations database
-#python manage.py makemigrations
+python manage.py makemigrations
 python manage.py migrate rfis
 python manage.py migrate database
 python manage.py migrate
