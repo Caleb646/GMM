c056e6a19762cc03dd15047646adb22e36c24757
Caleb <calebthomas646@yahoo.com>
2022-04-29 11:24:47 -0500

    404s now redirect to the homepage

--- rfis/views/errors.py
+++ rfis/views/errors.py
@@ -3,16 +3,18 @@ from django.urls import reverse
 
 
 def bad_request(request, exception=None):
-    return render(request, "errors/404.html")
+    return render(request, "errors/403.html")
 
 
 def permission_denied(request, exception=None):
-    return render(request, "errors/404.html")
+    return render(request, "errors/403.html")
 
 
-def page_not_found(request, exception=None):
-    return render(request, "errors/404.html")
+def page_not_found(request, exception=None):  # redirect to homepage
+    return redirect(reverse("user:index"))
+    # return render(request, "errors/404.html")
 
 
-def server_error(request, exception=None):
-    return render(request, "errors/404.html")
+def server_error(request, exception=None):  # redirect to homepage
+    return redirect(reverse("user:index"))
+    # return render(request, "errors/404.html")
